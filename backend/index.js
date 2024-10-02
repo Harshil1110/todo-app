05731f467f9b641f8bdb734b8d1ed1e446f0bef4
Harshil Rana <ranaharshil557@gmail.com>
2024-10-02 22:42:38 +0530

    build

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
-const expres = require("express");
-const app = expres();
+const express = require("express");
+const app = express();
 require("./db");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
+const path = require('path');
 const todo = require("./routes/ToDo");
 const user = require("./routes/User");
-app.use(expres.json());
+app.use(express.json());
 app.use(
   cors({
     credentials: true,
@@ -15,6 +16,11 @@ app.use(
 );
 app.use("/todo", todo);
 app.use("/user", user);
+app.use(express.static(path.join(__dirname, "build")));
+// index file path
+app.get("/*", (req, res) => {
+  return res.status(200).sendFile(__dirname + "/build/index.html");
+});
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
